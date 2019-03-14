01d379870aeede0d2489ec80e216740b74ecd9e9
rosalux161 <info@janluetzler.de>
2019-03-14 11:10:10 +0100

    it61

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.13.37/';
+$config['base_url'] = 'http://it61.janluetzler.de/';
 
 /*
 |--------------------------------------------------------------------------
