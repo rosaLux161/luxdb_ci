94089ebe2e27e7541d86b679644c300bed3d31ac
rosalux161 <info@janluetzler.de>
2019-03-15 14:04:13 +0100

    updated home

--- application/views/home.php
+++ application/views/home.php
@@ -1,4 +1,2 @@
 <h2>Sie haben folgende Möglichkeiten:</h2>
-<a href="<?php echo site_url('customer/search');?>">Kunden suchen</a><br>
-<a href="<?php echo site_url('customer/add');?>">Kunden hinzufügen</a><br>
-<a href="<?php echo site_url('customer/delete');?>">Kunden löschen (not yet implemented)</a>
+<a href="<?php echo site_url('customer/search');?>">Kundenverwaltung</a><br>
