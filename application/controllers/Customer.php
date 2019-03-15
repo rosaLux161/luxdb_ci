1e06c3512c1c0cdd165a7212306f5196b9c74a7c
rosalux161 <info@janluetzler.de>
2019-03-15 12:59:23 +0100

    delete index from cust controller

--- application/controllers/Customer.php
+++ application/controllers/Customer.php
@@ -3,16 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Customer extends CI_Controller {
     ############################################
-    #   Startseite anzeigen
-    ############################################
-	public function index()
-	{
-        $data['page_title'] = 'Startseite';
-        $this->load->view('templates/header', $data);
-        $this->load->view('home');
-        $this->load->view('templates/footer');
-    }
-    ############################################
     #   Methode zum Hinzufügen eines Kunden
     ############################################
     public function add($site = ""){
