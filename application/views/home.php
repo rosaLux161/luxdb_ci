a1c9a01ad2186497e44e7e70304f88774532fcbb
rosalux161 <info@janluetzler.de>
2019-03-16 10:46:53 +0100

    changed home

--- application/views/home.php
+++ application/views/home.php
@@ -1,2 +1,3 @@
 <h2>Sie haben folgende Möglichkeiten:</h2>
-<a href="<?php echo site_url('customer/search');?>">Kundenverwaltung</a><br>
+<a href="<?php echo site_url('customer/search');?>">Kundensuche</a><br>
+<a href="<?php echo site_url('customer/add');?>">Kunde hinzufügen</a><br>
